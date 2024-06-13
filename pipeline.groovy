ea18596e70ad039ffe5408d0ad15c57df3bcdd8a
SamarMatoussi <148507480+SamarMatoussi@users.noreply.github.com>
2024-06-13 10:52:59 +0100

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,50 +1,54 @@
 pipelineJob('pipeline') {
-    definition {
-        cps {
-            script('''
-            pipeline {
-                agent {
-                    docker {
-                        image 'pipeline/jenkins'
-                        args '-u 0:0 -v /var/run/docker.sock:/var/run/docker.sock'
-                    }
+  definition {
+    cps {
+      script(
+'''pipeline {
+    agent {
+    tools {
+        maven 'maven'
+    }
+        docker {
+            image 'pipeline/jenkins'
+            args '-u 0:0 -v /var/run/docker.sock:/var/run/docker.sock'
+        }
+    }
+    environment {
+        registry = "samarmatoussi/jcasc"
+        registryCredential = 'dockerHub'
+        dockerImage = ""
+    }                    
+    stages {
+        stage('git') {
+            steps {
+                echo 'Cloning the repository'
+                git branch: 'master', credentialsId: 'github-credentials', url: 'https://github.com/SamarMatoussi/docker-spring-boot.git'
+            }
+        }
+        stage('MVN CLEAN'){
+            steps {
+            sh 'mvn clean'
+            }
+        }
+       
+        stage('Build Image') {
+            steps {
+                script {
+                    dockerImage = docker.build("${registry}:$BUILD_NUMBER")
                 }
-                environment {
-                    registry = "samarmatoussi/jcasc"
-                    registryCredential = 'dockerHub'
-                    dockerImage = ""
-                }
-                stages {
-                    stage('git') {
-                        steps {
-                            echo 'Cloning the repository'
-                            git branch: 'master', credentialsId: 'github-credentials', url: 'https://github.com/SamarMatoussi/docker-spring-boot.git'
-                        }
-                    }
-                    stage('MVN CLEAN') {
-                        steps {
-                            sh 'export PATH=$PATH:/opt/maven/bin && mvn clean install'
-                        }
-                    }
-                    stage('Build Image') {
-                        steps {
-                            script {
-                                dockerImage = docker.build("${registry}:$BUILD_NUMBER")
-                            }
-                        }
-                    }
-                    stage('Deploy Image') {
-                        steps {
-                            script {
-                                docker.withRegistry('', registryCredential) {
-                                    dockerImage.push()
-                                }
-                            }
-                        }
+            }
+        }
+        stage('Deploy Image') {
+            steps {
+                script {
+                    docker.withRegistry('', registryCredential) {
+                        dockerImage.push()
                     }
                 }
             }
-            ''')
         }
+        
     }
+}''')
+    }
+  }
 }
